0f72009c1e16fc9e31d3e3df0596a967bdd5341d
Matías Fuentes <matiasf.cortez@gmail.com>
2021-10-10 23:50:40 -0300

    msemaquetación finalizada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import './styles.css';
 
-const urlApi = 'https://api.github.com/search/users?q=nicolas&per_page=10';
+$.create = (element) => document.createElement(element);
+
+const fetchUsersURL = (name) => `https://api.github.com/search/users?q=${name}&per_page=9`;
+
+const userCardTemplate = (profileImg, name, followers, repos = 0) =>
+	` <div class="card"> 
+
+<img class="card-image" src="${profileImg}">
+<div  class="title card-title">${name}</div>
+
+<div class="card-item-container">
+    <div class="card-item">
+        <div class=""><i class="fas fa-users"></i>Follows</div>
+        <div class="">${followers}</div>
+    </div>
+
+    <div class="card-item">
+        <div><i class="fas fa-book"></i>Repos</div>
+        <div>${repos}</div>
+    </div>
+
+
+</div>
+
+</div>
+
+`;
+
+const displayCards = (name) => {
+	$.get(fetchUsersURL(name), (data) => {
+		const usersList = data.items;
+
+		for (let user of usersList) {
+			const userName = user.login;
+
+			$.get(`https://api.github.com/users/${userName}`, (userData) => {
+				const a = $.create('a');
+				const div = $.create('div');
+				const userProfileImg = userData.avatar_url;
+				const userRepos = userData.public_repos;
+				const userFollowers = userData.followers;
+				a.setAttribute('href', user.html_url);
+				a.setAttribute('target', '__blank');
+				a.innerHTML = userCardTemplate(userProfileImg, userName, userFollowers, userRepos);
+
+				$('.card-container').append(a);
+			});
+		}
+	});
+};
+
+const searchUsers = () => {
+	$('.button').click(() => {
+		const inputTextValue = $('.text-input').val();
+		$('.card-container').html('');
+		displayCards(inputTextValue);
+	});
+
+	$('.text-input').keyup((e) => {
+		if (e.keyCode == '13') {
+			const inputTextValue = $('.text-input').val();
+			$('.card-container').html('');
+			displayCards(inputTextValue);
+		}
+	});
+};
+
+searchUsers();
